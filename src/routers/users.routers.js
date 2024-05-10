628419af2eea53a82e3e5b79456b87b6201cd50d
Magnifique UMURERWA <ketymagnifique@gmail.com>
2024-05-10 13:47:07 +0300

    change

--- src/routers/users.routers.js
+++ src/routers/users.routers.js
@@ -5,10 +5,13 @@ const userRouter = express.Router();
 import { SignUp, SignIn, ValidateOpt, ForgotPassword, ResetPassword} from '../controllers/users.controller.js';
 import { signUpValidations, signInValidations, otpValidation, forgotPasswordValidation, resetPasswordValidation } from '../utils/validation.js';
 import configuration from '../configs/index.js'
+import { getRoles,addRole} from '../controllers/role.contoller.js';
 import cors from 'cors'
 import session from 'express-session'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import passport from 'passport'
+userRouter.get('/role',getRoles)
+userRouter.post('/add',addRole)
 userRouter.post('/signup', signUpValidations, SignUp);
 userRouter.post('/signin', signInValidations, SignIn);
 userRouter.post('/verify', otpValidation, ValidateOpt);
@@ -22,8 +25,6 @@ userRouter.use(session({
 }))
 
 userRouter.get('/auth/google',
-
-
 
   passport.authenticate('google', { scope: ['profile', 'email'] }));
 

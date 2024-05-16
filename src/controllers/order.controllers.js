71e98e6758eaaff76f99056713c1a73a08abd267
Umwizer <ruthumwizerwa@gmail.com>
2024-05-16 10:23:13 +0200

    adding functionalities in order

--- src/controllers/order.controllers.js
+++ src/controllers/order.controllers.js
@@ -4,26 +4,7 @@ import asyncWrapper from "../middlewares/async.js";
 import { NotFoundError, BadRequestError } from "../errors/index.js";
 import { validationResult } from "express-validator";
 import { sendEmail } from "../utils/sendEmail.js";
-import stockModel from "../models/stock.model.js"; 
-
-// Function to fetch product details based on product name
-async function getProductDetails(productName) {
-  try {
-    const stockItem = await stockModel.findOne({ NameOfProduct: productName });
-
-    if (!stockItem) {
-      return null;
-    }
-
-    return {
-      Description: stockItem.Description,
-      pricePerTon: stockItem.pricePerTon,
-    };
-  } catch (error) {
-    console.error('Error fetching product details:', error);
-    throw error;
-  }
-}
+import stockModel from "../models/stock.model.js";
 
 export const addOrder = asyncWrapper(async (req, res, next) => {
   try {
@@ -32,39 +13,34 @@ export const addOrder = asyncWrapper(async (req, res, next) => {
     if (missingFields.length > 0) {
       throw new BadRequestError(`Missing required fields: ${missingFields.join(', ')}`);
     }
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const errorMessages = errors.array().map(error => error.msg);
       throw new BadRequestError(errorMessages.join(', '));
     }
-
     const { selectedStockItems, phoneNumber, shippingAddress } = req.body;
     const customer = req.user._id;
-
     const user = await userModel.findById(customer);
     if (!user) {
       throw new NotFoundError('User not found');
     }
-
     let totalAmount = 0;
-
     const updatedStockItems = await Promise.all(selectedStockItems.map(async (item) => {
-      const productDetails = await getProductDetails(item.NameOfProduct);
-      if (!productDetails) {
-        throw new NotFoundError(`Product details not found for product: ${item.NameOfProduct}`);
+      const product = await stockModel.findOne({ NameOfProduct: item.NameOfProduct });
+      if (!product) {
+        throw new NotFoundError(`Product not found: ${item.NameOfProduct}`);
       }
-      const itemTotalPrice = item.quantity * productDetails.pricePerTon;
+      const itemTotalPrice = item.quantity * product.pricePerTon;
       totalAmount += itemTotalPrice;
-
       return {
-        ...item,
-        Description: productDetails.Description,
-        pricePerTon: productDetails.pricePerTon,
+        _id: product._id, // Use product ID instead of name
+        NameOfProduct: product.NameOfProduct,
+        Description: product.Description,
+        pricePerTon: product.pricePerTon,
+        quantity: item.quantity,
         itemTotalPrice
       };
     }));
-
     const newOrder = new orderModel({
       customer,
       selectedStockItems: updatedStockItems,
@@ -73,17 +49,19 @@ export const addOrder = asyncWrapper(async (req, res, next) => {
       totalAmount,
       totalItems: selectedStockItems.reduce((total, item) => total + item.quantity, 0),
     });
-
     await newOrder.save();
-
+    await Promise.all(updatedStockItems.map(async (item) => {
+      const stock = await stockModel.findOne({ NameOfProduct: item.NameOfProduct });
+      if (stock) {
+        stock.quantity -= item.quantity;
+        await stock.save();
+      }
+    }));
     const recipientEmail = user.email;
     const subject = 'Your order has been received';
     const body = `Dear ${user.email},
-
 Your order has been received successfully.
-
 Here are the details of your order:
-
 ${updatedStockItems.map((item, index) => `
 Item ${index + 1}:
 Name: ${item.NameOfProduct}
@@ -92,27 +70,20 @@ Quantity: ${item.quantity}
 Unit Price: ${item.pricePerTon}
 Total Price: ${item.itemTotalPrice}
 `).join('\n')}
-
 Total Amount: ${totalAmount}
 Total Items: ${newOrder.totalItems}
 Shipping Address:
 ${shippingAddress}
-
 Total Amount to be Paid: ${totalAmount}
-
 Thank you for your order!
-
 Sincerely,
 Agriconnect`;
-
     await sendEmail(recipientEmail, subject, body);
-
     res.status(201).json({ success: true, data: newOrder });
   } catch (error) {
     next(error);
   }
 });
-
 export const getAllOrders = asyncWrapper(async (req, res, next) => {
   try {
     const orders = await orderModel.find();
